dd97edc69944f2ca6cbf03a1a2d7c8fa69b79c0c
nazyhbada-elavi <145340037+nazyhbada-elavi@users.noreply.github.com>
2025-08-06 09:45:10 +0100

    Build Sidebar Component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="h-screen flex bg-secondary-50">
+    <!-- Sidebar -->
+    <Sidebar />
 
     <!-- Main Content -->
     <div class="flex-1 flex">
@@ -7,17 +9,20 @@
       <div class="flex-1">
         <TranslationView />
       </div>
+      
+      <!-- Glossary Panel -->
+      <div v-if="isGlossaryVisible" class="w-80 border-l border-secondary-200">
+        <GlossaryPanel />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import Sidebar from './components/Sidebar.vue';
 import TranslationView from './components/TranslationView.vue';
+import GlossaryPanel from './components/GlossaryPanel.vue';
+import { useGlossary } from './composables/useGlossary';
 
-const isGlossaryVisible = ref(false);
-
-const toggleGlossary = () => {
-  isGlossaryVisible.value = !isGlossaryVisible.value;
-};
+const { isGlossaryVisible } = useGlossary();
 </script>
