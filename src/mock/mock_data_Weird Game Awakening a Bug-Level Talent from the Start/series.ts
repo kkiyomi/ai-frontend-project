16e2b810aafae0e508a717e56208771f53e7472f
kkiyomi <kkiyomi902@gmail.com>
2025-10-12 23:41:48 +0100

    [ADD] mock data

---
+++ src/mock/mock_data_Weird Game Awakening a Bug-Level Talent from the Start/series.ts
@@ -0,0 +1,38 @@
+// Auto-generated mock data for Weird Game Awakening a Bug-Level Talent from the Start
+import type { Series, Chapter, GlossaryTerm } from '@/types';
+
+export const mockSeries: Series[] = [
+  {
+    "id": "21ca339b4abe",
+    "name": "Weird Game Awakening a Bug-Level Talent from the Start",
+    "description": "Auto-generated data for Weird Game Awakening a Bug-Level Talent from the Start",
+    "createdAt": "2025-10-05T20:55:27.211558",
+    "chapterIds": [
+      "ac18c6d53fc7",
+      "ea85507354ea",
+      "73cbcc3206b5",
+      "7ec2ea9ba0bd",
+      "8ca2b395e09e",
+      "544e3e3fa22a",
+      "4fdbc9269433",
+      "6f040f8b0f78",
+      "8feeafb4dc2d",
+      "cacb5414d268",
+      "a985b921f964",
+      "e305515d68ff",
+      "53a0a98e8b75",
+      "25e3d5003ab6",
+      "208a72789aef",
+      "5af9f0c04def",
+      "037866f428ab",
+      "cbae3f439d54",
+      "d63eeddb579a",
+      "1905288ed807",
+      "90b18c26d8e3",
+      "5a745ca80905",
+      "ee2d0029b303",
+      "81276cdaf5ba",
+      "8549425b5e6e"
+    ]
+  }
+];
