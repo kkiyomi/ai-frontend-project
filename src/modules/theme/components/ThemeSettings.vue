83a5f94d7ea801eefa472087360c05efd0edcf5a
Nazyh KK <kkiyomi902@gmail.com>
2026-04-15 21:29:03 +0100

    upd

--- src/modules/theme/components/ThemeSettings.vue
+++ src/modules/theme/components/ThemeSettings.vue
@@ -116,7 +116,7 @@
       <div class="text-center p-6 bg-base-200 rounded-lg border border-base-300 shadow-xl max-w-sm mx-4 cursor-pointer"
         @click="openUpgradeModal"
       >
-        <svg class="w-12 h-12 text-warning mx-auto mb-4" fill="currentColor" viewBox="0 0 20 20">
+        <svg class="w-12 h-12 text-base-content mx-auto mb-4" fill="currentColor" viewBox="0 0 20 20">
           <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
         </svg>
         <h4 class="text-lg font-medium text-base-content mb-2">Themes Locked</h4>
